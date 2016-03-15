35433eef7c75990e2bbec0864790b0017a488f7f
glmiyamoto <glmiyamoto@gmail.com>
2016-03-14 22:45:18 -0300

    Ajuste no menu de navegação

--- app/src/main/java/br/com/glmiyamoto/vendasdemo/MainActivity.java
+++ app/src/main/java/br/com/glmiyamoto/vendasdemo/MainActivity.java
@@ -43,45 +43,25 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_my_sales) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_my_products) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_affiliates) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_messages) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_notifications) {
+
+        } else if (id == R.id.nav_my_account) {
+
+        } else if (id == R.id.nav_about_apps) {
 
         }
 
